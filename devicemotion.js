88fa8ba5685b110d947468154004a50491b7fdf3
mschneider4 <40787186+mschneider4@users.noreply.github.com>
2018-07-03 09:38:07 +0200

    Add files via upload

---
+++ devicemotion.js
@@ -0,0 +1,10 @@
+if (window.DeviceMotionEvent) {
+    document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
+    window.addEventListener('devicemotion', function(event) {
+        document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
+        document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
+        document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
+    });
+} else {
+    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
+}
